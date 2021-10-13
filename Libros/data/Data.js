c4d7def0cb24e032c5a579eaeaf704c7858a65d8
xDarthSides <70679146+xDarthSides@users.noreply.github.com>
2021-10-12 22:46:43 -0600

    Pagina de Inicio

---
+++ Libros/data/Data.js
@@ -0,0 +1,17 @@
+export const dummyData =
+        [{
+                title: 'La aplicacion perfecta para los lectores', url: 'https://www.welivesecurity.com/wp-content/uploads/2019/04/libros-seguridad-informacion.jpg',
+                description: "Descrubre nuestros productos en nuestar tienda online",
+                id: 1
+
+        },
+        {
+                title: 'Ofertas', url: 'https://cnnespanol.cnn.com/wp-content/uploads/2021/03/210303111324-dr-seuss-books-library-full-169.jpg?quality=100&strip=info',
+                description: "Ya vistes nuestras nuevas oferas? Descubrelas",
+                id: 2
+        },
+        {
+                title: 'Recomendaciones', url: 'http://www.libropatas.com/wp-content/uploads/2016/11/libros-ofertas-black-friday.jpeg',
+                description: "Descubre los libros que hemos recomendado solo para ti",
+                id: 3
+        }]
